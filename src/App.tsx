e1519a8ee98528984a1b144696d3f8b6ce0f2acc
Naveen <68naveendwivedi@gmail.com>
2023-12-21 00:07:03 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import MainPage from './MainPage/MainPage';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <MainPage/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="card fixed-bottom">
+    <div className="card-body">
+      <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  </div>
+  );
+}
+
+export default Footer;
